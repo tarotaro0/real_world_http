5214fe8825e7ec889b4443e10ab8d38578a8b5ec
tarotaro0 <tarousann11922960@yahoo.co.jp>
2019-02-18 17:08:43 +0900

    Add digest authorization

--- program/test_echo_server.go
+++ program/test_echo_server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/k0kubun/pp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +20,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hellooooo</body></html>\n") // responseに書き込み
 }
 
+// Digest 認証
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %v\n", r.Header)
+	defer r.Body.Close()
+
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok { // Authorizationヘッダーがない場合に401error
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="TgLc25U2BQA=f510a2780473e18e6587be702c2e67fe2b04afd", algorithm=MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler) // path "/" にアクセス時、handler()をcall
+	http.HandleFunc("/digest", handlerDigest)
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"               // 18888 portに設定
 	log.Println(httpServer.ListenAndServe()) // 設定したportで待ち受け、handlerを呼び出すことを開始
